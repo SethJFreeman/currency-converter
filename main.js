af399481ce66b8b6e20c456600876c0885da8245
Seth J. Freeman <cyanidecylinder@gmail.com>
2018-10-04 17:59:58 -0400

    added comments

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-function euroConverter(){
+function euroConverter(){ // Converts European Euro
   document.converter.dollar.value = document.converter.euro.value * 1.470
   document.converter.pound.value = document.converter.euro.value * 0.717
   document.converter.yen.value = document.converter.euro.value * 165.192
 }
-function dollarConverter(){
+function dollarConverter(){ // Converts American Dollar
   document.converter.euro.value = document.converter.dollar.value * 0.680
   document.converter.pound.value = document.converter.dollar.value * 0.488
   document.converter.yen.value = document.converter.dollar.value * 112.36
 }
-function poundConverter(){
+function poundConverter(){ // Converts British Pound
   document.converter.dollar.value = document.converter.pound.value * 2.049
   document.converter.euro.value = document.converter.pound.value * 1.394
   document.converter.yen.value = document.converter.pound.value * 230.27
 }
-function yenConverter(){
+function yenConverter(){ // Converts Japanese Yen
   document.converter.dollar.value = document.converter.yen.value * 0.0089
   document.converter.pound.value = document.converter.yen.value * 0.00434
   document.converter.euro.value = document.converter.yen.value * 0.00605
